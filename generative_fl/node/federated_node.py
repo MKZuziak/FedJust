9e5b6dd59479fe04372a5e7fab547866127b13e3
Scolpe <63779111+Scolpe@users.noreply.github.com>
2024-02-15 15:54:20 +0100

    Adds complete federated node

--- generative_fl/node/federated_node.py
+++ generative_fl/node/federated_node.py
@@ -1,1 +1,179 @@
+from typing import Any
+from functools import partial
+from collections import OrderedDict
 import copy
+
+import torch
+
+from generative_fl.model.federated_model import FederatedModel
+
+# import torch
+# from numpy import array
+# from numpy.random import default_rng
+# from datasets import arrow_dataset
+# from forcha.models.federated_model import FederatedModel
+# from forcha.utils.loggers import Loggers
+# from forcha.utils.helpers import find_nearest
+# from forcha.components.settings.settings import Settings
+
+# node_logger = Loggers.node_logger()
+
+class FederatedNode:
+    def __init__(self) -> None:
+        """An abstract object representing a single node in the federated training.
+        
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        None
+        """
+        self.node_id = None
+        self.model = None
+    
+    
+    def connect_data_id(
+        self,
+        node_id: int | str,
+        model: FederatedModel,
+        data: Any,
+        orchestrator: bool = False
+    ) -> None:
+        """Attaches dataset and id to a node, creating an individualised version.
+        
+        Parameters
+        ----------
+        node_id: int | str
+            ID of the node
+        model: FederatedModel
+            FederatedModel template that should be copied and attached to the node
+        data: Any
+            Data to be attached to a FederatedModel.
+        orchestrator: bool (default to False)
+            Boolean flag if the node is belonging to the orchestrator
+        Returns
+        -------
+        None
+        """
+        self.node_id = node_id
+        self.model = copy.deepcopy(model)
+        self.model.attach_dataset_id(
+            local_dataset=data,
+            node_name=self.node_id,
+            only_test=orchestrator)
+    
+    
+    def get_weights(self) -> OrderedDict:
+        """Extended API call to recover model weights. Causes the same effect as calling
+        node.model.get_weigts()
+        Parameters
+        ----------
+        None
+        Returns
+        OrderedDict
+        None
+        """
+        return self.model.get_weights()
+    
+    
+    def update_weights(self,
+                       weights: OrderedDict) -> None:
+        """Extended API call to update model. Causes the same effect as calling
+        node.model.update_weights()
+        Parameters
+        ----------
+        weights:OrderedDict
+            OrderedDict to be uploaded onto the model.
+        Returns
+        None
+        """
+        self.model.update_weights(weights)
+            
+
+    def train_local_model(self,
+                          iteration: int,
+                          local_epochs: int,
+                          mode: str = 'weights',
+                          save_model: bool = False,
+                          save_path: str = None) -> tuple[int, OrderedDict, list[float], list[float]]:
+        """This function starts the server phase of the federated learning.
+        In particular, it trains the model locally and then sends the weights.
+        Then the updated weights are received and used to update
+        the global model.
+        
+        Parameters
+        ----------
+        node: FederatedNode 
+            Node that we want to train.
+        iteration: int
+            Current global iteration
+        local_epochs: int
+            Number of local epochs for which the node should be training.
+        mode: str (default to 'weights')
+            Mode = 'weights': Node will return model's weights.
+            Mode = 'gradients': Node will return model's gradients.
+        save_model: bool (default to False)
+            Boolean flag to save a model.
+        save_path: str (defualt to None)
+            Path object used to save a model
+        
+        Returns
+        -------
+            Tuple[int, OrderedDict, List[float], List[float]]:
+        """
+        # node_logger.info(f"[ITERATION {iteration} | NODE {self.node_id}] Starting training on node {self.node_id}")
+        loss_list: list[float] = []
+        accuracy_list: list[float] = []
+        
+        if mode == 'gradients':
+            raise NotImplementedError()
+            self.model.preserve_initial_model()
+        
+        for epoch in range(local_epochs):
+            metrics = self.local_training(
+                iteration=iteration, 
+                epoch=epoch
+                )
+            loss_list.append(metrics["loss"])
+            accuracy_list.append(metrics["accuracy"])
+        if save_model:
+            self.model.store_model_on_disk(
+                iteration=iteration, 
+                path=save_path
+                )
+        # node_logger.info(f"[ITERATION {iteration} | NODE {self.node_id}] Results of training on node {self.node_id}: {accuracy_list}")
+        if mode == 'weights':
+            return (
+                self.node_id,
+                self.model.get_weights(),
+                loss_list,
+                accuracy_list
+                )
+        elif mode == 'gradients':
+            return NotImplementedError
+            return (
+                self.node_id,
+                self.model.get_gradients(),
+                loss_list,
+                accuracy_list
+                )
+    
+    
+    def local_training(self,
+                       iteration: int,
+                       epoch: int
+                       ) -> dict[int, int]:
+        """Helper method for performing one epoch of local training.
+        Performs one round of Federated Training and pack the
+        results (metrics) into the appropiate data structure.
+        
+        Parameters
+        ----------
+    
+        Returns
+        -------
+            dict[int, int]: metrics from the training.
+        """
+        loss, accuracy = self.model.train(iteration=iteration, epoch=epoch)
+        return {"loss": loss, "accuracy": accuracy}
--- tests/unit_test/node/federated_node_tests.py
+++ tests/unit_test/node/federated_node_tests.py
@@ -1,1 +1,145 @@
-from generative_fl.node.federated_node import *
+import unittest
+from functools import partial
+from collections import OrderedDict
+import os
+
+import torch
+
+from generative_fl.node.federated_node import FederatedNode
+from generative_fl.model.federated_model import FederatedModel
+from tests.test_props.datasets import return_mnist
+from tests.test_props.nets import NeuralNetwork
+
+
+class NodeTests(unittest.TestCase):
+
+    def test_init(self):
+        # Initialization
+        node = FederatedNode()
+        # Tests
+        self.assertIsNotNone(node)
+    
+    
+    def test_attach_dataset(self):
+        # Initializaiton
+        node = FederatedNode()
+        train, test = return_mnist()
+        # Preliminary tests
+        self.assertIsNone(node.node_id)
+        self.assertIsNone(node.model)
+        model = NeuralNetwork()
+        optimizer_template = partial(torch.optim.SGD,lr=0.001)
+        model_prop = FederatedModel(
+            net=model,
+            optimizer_template=optimizer_template,
+            loader_batch_size=32,
+            force_cpu=True)
+        node.connect_data_id(
+            node_id = 42,
+            model=model_prop,
+            data=[train, test]
+        )
+        
+        # Tests
+        self.assertIsNotNone(node.node_id)
+        self.assertIsNotNone(node.model)
+    
+    
+    def test_train_node(self):
+        # Initializaiton
+        node = FederatedNode()
+        train, test = return_mnist()
+        # Preliminary tests
+        self.assertIsNone(node.node_id)
+        self.assertIsNone(node.model)
+        model = NeuralNetwork()
+        optimizer_template = partial(torch.optim.SGD,lr=0.001)
+        model_prop = FederatedModel(
+            net=model,
+            optimizer_template=optimizer_template,
+            loader_batch_size=32,
+            force_cpu=True)
+        node.connect_data_id(
+            node_id = 42,
+            model=model_prop,
+            data=[train, test]
+        )
+        # Training
+        path = os.getcwd()
+        # Removing saved model
+        iteration = 21
+        epochs = 3
+        (node_id, weights, acc, loss) = node.train_local_model(iteration=iteration,
+                               local_epochs=epochs,
+                               mode='weights',
+                               save_model=True,
+                               save_path=path)
+        #Tests
+        self.assertTrue(os.path.exists(os.path.join(path, "node_42_iteration_21.pt")))
+        self.assertEqual(type(node_id), int)
+        self.assertEqual(type(weights), OrderedDict)
+        self.assertEqual(type(acc), list)
+        self.assertEqual(type(loss), list)
+        self.assertEqual(type(node_id), type(42))
+        
+        # Cleaning
+        os.remove(os.path.join(path, "node_42_iteration_21.pt"))
+    
+    
+    def test_receive_update_weights(self):
+        # Initializaiton
+        node = FederatedNode()
+        orchestrator = FederatedNode()
+        train, test = return_mnist()
+        # Preliminary tests
+        self.assertIsNone(node.node_id)
+        self.assertIsNone(node.model)
+        self.assertIsNone(orchestrator.node_id)
+        self.assertIsNone(orchestrator.model)
+        model = NeuralNetwork()
+        optimizer_template = partial(torch.optim.SGD,lr=0.001)
+        # Note Initialization
+        model_prop = FederatedModel(
+            net=model,
+            optimizer_template=optimizer_template,
+            loader_batch_size=32,
+            force_cpu=True)
+        node.connect_data_id(
+            node_id = 42,
+            model=model_prop,
+            data=[train, test]
+        )
+        # Orchestrator Initialization
+        orchestrator_prop = FederatedModel(
+            net=model,
+            optimizer_template=optimizer_template,
+            loader_batch_size=32,
+            force_cpu=False
+        )
+        orchestrator.connect_data_id(
+            node_id='orchestrator',
+            model=orchestrator_prop,
+            data = [test],
+            orchestrator=True)
+        
+        # Weights retireve
+        weights_node = node.get_weights()
+        weights_orchestrator = orchestrator.get_weights()
+        # Tests -> weights should be the same
+        for (layer_node, layer_orchestrator) in zip(weights_node.values(), weights_orchestrator.values()):
+            self.assertTrue(torch.allclose(layer_node, layer_orchestrator))
+        
+        # Fake weights generation and upload
+        fake_weights = OrderedDict()
+        for key, layer in weights_node.items():
+            fake_weights[key] = torch.rand(layer.shape)
+        node.update_weights(fake_weights)
+        weights_node = node.get_weights()
+        weights_orchestrator = orchestrator.get_weights()
+        # Test -> weights should not be the same
+        for (layer_node, layer_orchestrator) in zip(weights_node.values(), weights_orchestrator.values()):
+            self.assertFalse(torch.allclose(layer_node, layer_orchestrator))
+        
+
+if __name__ == "__main__":
+    unittest.main()
