29b21c6091bc242ed21ed49ed2078e5cbd1dea8f
Scolpe <63779111+Scolpe@users.noreply.github.com>
2024-02-16 12:22:49 +0100

    Adds basic aggregator class

--- generative_fl/aggregators/aggregator.py
+++ generative_fl/aggregators/aggregator.py
@@ -1,4 +1,34 @@
+from collections import OrderedDict
+import copy
+
+import torch
+
 class Aggregator():
     """Basic class for all Federated Aggregators"""
     def __init__(self) -> None:
         pass
+    
+    
+    def aggregate_weights(weights: dict[int: OrderedDict]) -> OrderedDict:
+        """Basic aggregate function (equal to FedAvg) that returns the aggregate version of the
+        weights. Perform deepcopy on the passed parameters.
+        
+        Parameters
+        ----------
+        weights: dict[int: OrderedDict]
+        
+        Returns
+        -------
+        OrderedDict"""
+        
+        results = OrderedDict()
+        for params in weights.values():
+            for key in params:
+                if results.get(key) is None:
+                    results[key] = copy.deepcopy(params[key]) #Here we could add copy and deepcopy
+                else:
+                    results[key] += copy.deepcopy(params[key])
+
+        for key in results:
+            results[key] = torch.div(results[key], len(weights))
+        return results
---
+++ tests/unit_test/aggregators/test_aggregator.py
@@ -0,0 +1,15 @@
+import unittest
+
+from generative_fl.aggregators.aggregator import Aggregator
+
+
+class Test_Aggregator(unittest.TestCase):
+    
+    def test_init(self):
+        pass
+        # TODO
+    
+    
+    def test_weight_aggregation(self):
+        pass
+        # TODO
