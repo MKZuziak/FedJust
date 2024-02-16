dce2e9262073928cc15c2716b2e59eafb5830d35
Scolpe <63779111+Scolpe@users.noreply.github.com>
2024-02-16 11:19:36 +0100

    Adds a handler for preserving the nested dict as csv

---
+++ generative_fl/files/handlers.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+def save_nested_dict_ascsv(
+    data: dict[dict],
+    save_path: str):
+    """Attaches models of the nodes to the simulation instance.
+        
+    Parameters
+    ----------
+    data: dict[dict]
+        Nested dict containing the data. Note that the external key will not be saved, only the 
+        data in the internal dictionary will be transformed to fieldnames and saved.
+    save_path: str
+        A full save path for preserving the dictionary. Must end with *.csv file extension.
+    Returns
+    -------
+    None
+    """
+    with open(save_path, "a", newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=next(iter(data.values())).keys())
+        # Checks if the file already exists, otherwise writer header
+        if os.path.getsize(save_path) == 0:
+            writer.writeheader()
+        for row in data.values():
+            writer.writerow(row)
---
+++ tests/unit_test/files/handlers_tests.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class Handlers_Tests(unittest.TestCase):
+    pass
+    # TODO
