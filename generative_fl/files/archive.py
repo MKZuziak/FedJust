b24e67c5465336e04903fa86ee754fe637dcc2e6
Scolpe <63779111+Scolpe@users.noreply.github.com>
2024-02-19 10:51:57 +0100

    Adds archive file and create_archive utility

---
+++ generative_fl/files/archive.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+
+def create_archive(
+    path: str,
+    archive_name: str = time.strftime("%d.%m.%Y %H:%M%S", time.gmtime())
+    ) -> tuple[str, str, str]:
+    """Creates a basic directory structure for preserving simulation files.
+    Returns Paths to metrics save location, nodes (models) save location and
+    orchestrator (models) save location.
+    
+    Parameters
+    ----------
+    path: str | Path
+        Path-like object to the root directory in which the archive should be created
+    archive_name: str (default to time.strftime("%d.%m.%Y %H:%M%S", time.gmtime())
+        Name of the archive.
+    
+    Returns
+    -------
+    tuple[str, str str]
+    """
+    root = os.path.join(path, archive_name)
+    assert not os.path.exists(root), f"Directory {root} already exists" 
+    
+    metrics_savepath = os.path.join(root, 'results')
+    nodes_models_savepath = os.path.join(root, 'models', 'orchestrator')
+    orchestrator_model_savepath = os.path.join(root, 'models', 'nodes')
+    os.makedirs(metrics_savepath)
+    os.makedirs(nodes_models_savepath)
+    os.makedirs(orchestrator_model_savepath)
+    
+    return(metrics_savepath, nodes_models_savepath, orchestrator_model_savepath) 
---
+++ tests/unit_test/files/archive_tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from generative_fl.files.archive import create_archive
+
+
+class Archive_Tests(unittest.TestCase):
+    
+    def test_create_archive(self):
+        pass
+        # TODO
+
+
+if __name__ == "__main__":
+    unittest.main()
